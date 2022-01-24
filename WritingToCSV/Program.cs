a57d635e9c96848a53ddefccce7eb8286eec3785
NiMa Mehdizadeh <nima.mehdizadeh@gmail.com>
2022-01-24 15:33:50 +0330

    Add project files.

---
+++ WritingToCSV/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+
+namespace WritingToCSV
+{
+    //https://code-maze.com/csharp-writing-csv-file/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myPersonObjects = new List<Person>()
+            {
+                new Person { Id = 1, IsLiving = true, Name = "John" },
+                new Person { Id = 2, IsLiving = true, Name = "Steve" },
+                new Person { Id = 3, IsLiving = true, Name = "James" }
+            };
+
+            using (var writer = new StreamWriter("filePersons.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(myPersonObjects);
+            }
+            Console.WriteLine("Done!");
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsLiving { get; set; }
+    }
+}
